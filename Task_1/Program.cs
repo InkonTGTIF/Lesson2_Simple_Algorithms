595b56cadb9c41de2079c5e2eb07300859b2427a
Karpov Kirill <maccdig@gmail.com>
2023-11-22 22:53:23 +0300

    add Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 161;
+
+if(a%7==0 && a%23==0){
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿int x = 1;
+int y = 1;
+
+if (x>0 && y>0){
+    Console.WriteLine($"№1");
+}
+else if (x<0 && y>0){
+    Console.WriteLine($"№2");
+}
+else if (x<0 && y<0){
+    Console.WriteLine($"№3");
+}
+else if (x>0 && y<0){
+    Console.WriteLine($"№4");
+}
