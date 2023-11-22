b83835188ad9522da987ad5d6451e5dde04fb5f9
Karpov Kirill <maccdig@gmail.com>
2023-11-22 23:06:08 +0300

    add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 61;
+
+int firstNumber = number /10;
+int secondNumer = number % 10;
+int maxNumber = number;
+
+if (number>99){
+    Console.WriteLine($"Число должно быть меньше 99");
+}
+else if(firstNumber<secondNumer){
+    Console.WriteLine($"{secondNumer}");
+}
+else {
+    Console.WriteLine($"{firstNumber}");
+}
