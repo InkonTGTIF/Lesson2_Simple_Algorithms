5daedb935a3ae0dc1778cc8cf203fad4249d627d
Karpov Kirill <maccdig@gmail.com>
2023-11-22 23:43:10 +0300

    add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = 8414;
+int resultNumber = 1;
+
+if (number<=10){
+    Console.WriteLine($"{number}");
+}
+while (number>0){
+    resultNumber = number%10;
+    number = number/10;
+    if (number>0){
+        Console.Write(resultNumber + ", ");
+    }
+    else{
+        Console.Write(resultNumber);
+    }
+}
